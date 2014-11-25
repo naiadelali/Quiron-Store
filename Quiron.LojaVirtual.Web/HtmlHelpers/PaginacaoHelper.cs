9034be2dad243a80dd96f809aeb54937a66dae37
naiadelima <naiade.lima@live.com>
2014-11-25 16:27:18 -0200

    paginacao

---
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+
+namespace Quiron.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelper
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> PaginacaoUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes.Add("href", PaginacaoUrl(i));
+
+                if (paginacao.PaginaAtual == i)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItemTotal { get; set; }
+        public int ItensPorPagina { get; set; }
+        public int PaginaAtual { get; set; }
+
+        public int TotalPagina
+        {
+            get { return (int)Math.Ceiling((decimal)ItemTotal / ItensPorPagina); }
+        }
+    }
+}
