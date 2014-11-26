f002fd71b9a93e05342e3f635a0b6836a1a406c6
naiadelima <naiade.lima@live.com>
2014-11-26 16:49:11 -0200

    Aula 12

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Web.Mvc;
+using System.Web;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.Models;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -10,13 +14,13 @@ namespace Quiron.LojaVirtual.UnitTest
         [TestMethod]
         public void Take()
         {
-            int[] numeros = {1, 5, 6, 9, 8, 4, 7, 10, 15, 20};
+            int[] numeros = { 1, 5, 6, 9, 8, 4, 7, 10, 15, 20 };
 
             var resultado = from num in numeros.Take(5) select num;
 
             int[] teste = { 1, 5, 6, 9, 8 };
 
-            CollectionAssert.AreEqual(resultado.ToArray(),teste);
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
 
         [TestMethod]
@@ -25,8 +29,35 @@ namespace Quiron.LojaVirtual.UnitTest
             int[] numeros = { 1, 5, 6, 9, 8, 4, 7, 10, 15, 20 };
             var resultado = from num in numeros.Take(5).Skip(3) select num;
 
-            int[] teste = {9, 8};
-            CollectionAssert.AreEqual(resultado.ToArray(),teste);
+            int[] teste = { 9, 8 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
+
+        [TestMethod]
+        public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+        {
+            //Arrange
+            HtmlHelper htmlHelper = null;
+            var paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItemTotal = 28,
+                ItensPorPagina = 10
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+            //Act
+            MvcHtmlString resultado = htmlHelper.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+
+            Assert.AreEqual(
+           @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+           + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+           + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+           );
+
+        }
+
     }
 }
--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace Quiron.LojaVirtual.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:null,
+                url:"Pagina{pagina}",
+                defaults: new { controller = "Vitrine", action = "listaProdutos" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vitrine", action = "listaProdutos", id = UrlParameter.Optional }
             );
         }
     }
