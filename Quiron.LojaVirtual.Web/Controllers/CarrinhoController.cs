993ced2a4468b3a1e9beb46f0b9d096dc4317c34
naiadelima <naiade.lima@live.com>
2014-12-04 15:23:18 -0200

    Aula 18

---
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        //
+        // GET: /Carrinho/
+        private ProdutoRepositorio _repositorio;
+
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            Produto produto = _repositorio.Produtos.
+                FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                ObterCarrinho().Adicionar(produto, 1);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            Produto produto = _repositorio.Produtos.
+                FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+            {
+                ObterCarrinho().Remover(produto);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            Carrinho carrinho = (Carrinho) Session["Carrinho"];
+
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            CarrinhoViewModel carrinho = new CarrinhoViewModel
+            {
+                Carrinho = ObterCarrinho(),
+                returnUrl = returnUrl
+
+            };
+            return View(carrinho);
+        }
+    }
+}
